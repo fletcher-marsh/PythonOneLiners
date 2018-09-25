ed89f27c9457e5ab393e715055ef4edae9a335df
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-09-25 14:07:37 -0400

    sweep.py + formatting

--- nearestWords.py
+++ nearestWords.py
@@ -5,6 +5,9 @@ words in the wordlist that either match the given word or can be obtained by cha
 words exist, the function returns an empty list.
 For example, nearestWords(['cats','snarf','carts','cat','bats','cbts','abcd'],'cats')
 should return ['cats','bats','cbts']. There are no repeats in the wordlist.
+
+Source: Fletcher
+233 characters
 '''
 
 nearestWords = lambda l, w: list(filter(lambda x: True in [True if w in inserts else False for inserts in [[x[:i] + c + x[i+1:] for c in string.ascii_lowercase] for i in range(len(x))]], list(filter(lambda x: len(x) == len(w), l))))
