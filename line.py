b1f36d9700b21c08cd281979246b791b88f87354
Fletcher Marsh <fletcher.r.marsh@gmail.com>
2018-12-12 13:31:28 -0500

    consolidate docs

--- line.py
+++ line.py
@@ -1,6 +1,1 @@
-'''
-Tkinter simple drawing
-Source: Me
-Characters: 219
-'''
 (lambda t:(lambda t, r: (lambda c, r: (c.pack(), c.create_line(100, 100, 400, 400, fill="blue"), r.mainloop()))((lambda t, r: t.Canvas(r, width=500, height=500))(t,r), r))(t, t.Tk()))((lambda: __import__("tkinter"))())
--- nearestWords.py
+++ nearestWords.py
@@ -1,14 +1,1 @@
-'''
-Write the function nearestWords(wordList, word) that takes a wordlist and a
-single word (all words in this problem will only contain lowercase letters). The function returns a list of all the
-words in the wordlist that either match the given word or can be obtained by changing one letter. If no such
-words exist, the function returns an empty list.
-For example, nearestWords(['cats','snarf','carts','cat','bats','cbts','abcd'],'cats')
-should return ['cats','bats','cbts']. There are no repeats in the wordlist.
-
-Source: Fletcher
-Characters: 233
-'''
-
-nearestWords = lambda l, w: list(filter(lambda x: True in [True if w in inserts else False for inserts in [[x[:i] + c + x[i+1:] for c in string.ascii_lowercase] for i in range(len(x))]], list(filter(lambda x: len(x) == len(w), l))))
-print(nearestWords(['cats','snarf','carts','cat','bats','cbts','abcd'],'cats'))
+lambda l, w: list(filter(lambda x: True in [True if w in inserts else False for inserts in [[x[:i] + c + x[i+1:] for c in string.ascii_lowercase] for i in range(len(x))]], list(filter(lambda x: len(x) == len(w), l))))
